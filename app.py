686e4b4106c6bd5eb0c831b34f0ee1b890c89667
supraditya <aditya300100@gmail.com>
2022-12-16 00:36:35 -0500

    Documentation complete

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ APP_CACHE=cache.open_cache()
 
 @app.route('/', methods=['POST', 'GET'])
 def github_login():
-
+    '''
+    Function handles the OAuth logic for github's API
+    '''
     if not github.authorized:
         return redirect(url_for('github.login'))
     else:
@@ -54,6 +56,9 @@ def github_login():
 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
+    '''
+    Function renders the result page with fetched and processed data
+    '''
     if request.method=='POST':
         ownername=APP_CACHE['account_info']['login']
         repo_name=request.form['reponame']
@@ -65,6 +70,7 @@ def result():
         branch_url=f"/repos/{ownername}/{APP_CACHE['repo_name']}/branches"
         branch_data=github.get(branch_url)
 
+        #If fetch worked
         if branch_data.ok:
             branch_data_json=branch_data.json()
             if cache.is_entry_in_cache(APP_CACHE, branch_data_json)==False:
@@ -78,28 +84,47 @@ def result():
                 APP_CACHE['branch_names']=branch_names
                 cache.save_cache(APP_CACHE)
         else:
+            #If fetch failed
             return render_template('result.html')  
         all_branch_commits_data=[]
 
         branch_commits_dict={}
+
+
         for branch_name in APP_CACHE['branch_names']:
             #Accessing individual branch commit history by inputting branch name as a query parameter into URL
             url=f"/repos/{ownername}/{repo_name}/commits?sha={branch_name}"
             branch_commits=github.get(url)
+
             if branch_commits.ok:
                 branch_commits_json=branch_commits.json()
                 branch_commits_sha_list=[]
+                #Making a list of commit SHAs present with the fetched branch data
                 for commit in branch_commits_json:
                     branch_commits_sha_list.append(commit["sha"])
+                #Added the list of branch commit SHAs into a dictionary, and then caching it
                 branch_commits_dict[branch_name]=branch_commits_sha_list
                 if cache.is_entry_in_cache(APP_CACHE, branch_commits_dict)==False:
                     APP_CACHE['branch_commits_dict']=branch_commits_dict
                     cache.save_cache(APP_CACHE) 
+
+                #Cleaning the fetched API data from the branch to only keep the required data
                 cleaned_branch_data=helpers.branch_data_cleaner(branch_commits_json, APP_CACHE)
+
+                #Appending the list to a list that will eventually contain the cleaned branch data for EVERY branch in
+                #the repository
                 all_branch_commits_data.append(cleaned_branch_data)
+
+        #Creating a directed-graph like structure (with additional data) out of the recently made list
         graph=helpers.adj_matrix_creator(all_branch_commits_data)
+
+        #Sorting the directed graph dictionary according to commit timestamps
         sorted_graph=helpers.graph_sorter(graph)
+
+        #Separating the additional information into separate dictionaries, creating a pure directed-graph
         node_branch_dict, node_children_dict, node_message_dict=helpers.graph_data_pruner(sorted_graph)
+
+        #Rendering data within result.html
         return render_template('result.html', graph=graph, node_message_data=node_message_dict, node_children_data=node_children_dict, node_branch_data=node_branch_dict, first_commit=list(node_branch_dict.keys())[0], branch_names=APP_CACHE["branch_names"])
 
 if __name__ == "__main__":
