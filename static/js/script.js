5595ee9c4a5b864a304f1fbe631f949f71212242
supraditya <aditya300100@gmail.com>
2022-12-15 21:23:37 -0500

    got children commit ids into js function

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,22 @@ function hoverHide(nodeId)
     commitInfoContainer=document.getElementById(infoContainerId)
     commitInfoContainer.style.display="none";
 }
+
+function createLines(parentnodeId)
+{
+    parentNode=document.getElementById(parentnodeId);
+    str=parentNode.outerHTML
+    str_start=str.search('name')
+    str_end=str.lastIndexOf(' ')
+    str_slice=str.substring(str_start, str_end)
+    str_slice=str_slice.substring(8, str_slice.length-3)
+    // Opening quote removal
+    str_slice=str_slice.replace("'", "");
+    // Closing quote removal
+    str_slice=str_slice.replace("'", "");
+    // Removing whitespaces
+    str_slice=str_slice.replace(" ", "");
+    children=str_slice.split(",")
+    console.log(children)
+
+}
